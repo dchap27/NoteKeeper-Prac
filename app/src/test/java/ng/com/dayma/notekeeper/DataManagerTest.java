7ed1aec9f5407f4014a361d47c76e20e98552d76
Ahmad <ahmadadelaja@gmail.com>
2019-07-04 17:05:23 +0100

    Add a unit test for createNewNote in DataManagerTest.java

---
+++ app/src/test/java/ng/com/dayma/notekeeper/DataManagerTest.java
@@ -0,0 +1,16 @@
+package ng.com.dayma.notekeeper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ahmad on 7/4/2019.
+ */
+public class DataManagerTest {
+    @Test
+    public void createNewNote() throws Exception {
+
+    }
+
+}
