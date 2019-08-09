d2d99018dde43975ab538668b2e4ae2765c11f14
Ahmad <ahmadadelaja@gmail.com>
2019-08-09 13:02:09 +0100

    Create an SQL database

---
+++ app/src/main/java/ng/com/dayma/notekeeper/NoteKeeperOpenHelper.java
@@ -0,0 +1,34 @@
+package ng.com.dayma.notekeeper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Ahmad on 8/9/2019.
+ */
+
+public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "NoteKeeper.db";
+    public static final int DATABASE_VERSION = 1;
+    public NoteKeeperOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // create courseinfo table and noteinfo table
+        db.execSQL(NoteKeeperDatabaseContract.CourseInfoEntry.SQL_CREATE_TABLE);
+        db.execSQL(NoteKeeperDatabaseContract.NoteInfoEntry.SQL_CREATE_TABLE);
+
+        DatabaseDataWorker worker = new DatabaseDataWorker(db);
+        worker.insertCourses();
+        worker.insertSampleNotes();
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
